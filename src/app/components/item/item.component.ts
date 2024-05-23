67f7c1c1945de9e98712b8ab71b02ec629e6fc5a
eduardoaalmeidaa <almeidaeduardu@gmail.com>
2024-05-23 18:58:55 -0300

    f1 commit 05/23/2024

---
+++ src/app/components/item/item.component.ts
@@ -0,0 +1,43 @@
+import { outputAst } from "@angular/compiler";
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  Output,
+} from "@angular/core";
+import { faPen, faTrash } from "@fortawesome/free-solid-svg-icons";
+import { Item } from "src/app/interfaces/iItem";
+
+@Component({
+  selector: "app-item",
+  templateUrl: "./item.component.html",
+  styleUrls: ["./item.component.css"],
+})
+export class ItemComponent implements OnInit, OnChanges, OnDestroy {
+  @Input() item!: Item;
+  @Output() emitindoItemParaEditar = new EventEmitter();
+  @Output() emitindoItemParaDeletar = new EventEmitter();
+
+  faPen = faPen;
+  faTrash = faTrash;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  ngOnChanges() {}
+
+  editarItem() {
+    this.emitindoItemParaEditar.emit(this.item);
+  }
+  deletarItem() {
+    console.log("estao tentando me calar");
+    this.emitindoItemParaDeletar.emit(this.item.id);
+  }
+  ngOnDestroy() {
+    console.log("conseguiram me calar");
+  }
+}
